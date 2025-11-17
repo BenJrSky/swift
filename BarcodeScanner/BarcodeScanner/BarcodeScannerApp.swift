e75d1f6a6bd5231174ca389335c2ae96bdac2c41
devBen <benjrsky@gmail.com>
2025-11-17 14:27:22 +0100

    - ok

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Benito Massidda on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
